c099abf246101dd76f8dc4cbc5000092213c4428
LIU XUAN <87209849liuxuan@gmail.com>
2024-08-15 17:13:13 +0800

    Saved untracked files before switching to lx branch

---
+++ db_schema.sql
@@ -0,0 +1,33 @@
+PRAGMA foreign_keys=ON;
+
+BEGIN TRANSACTION;
+
+-- Table: User
+CREATE TABLE IF NOT EXISTS User (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    userpassword VARCHAR(50) NOT NULL,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+--- Table: Review
+CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    staffName TEXT NOT NULL,
+    rating INT,
+    feedback TEXT NOT NULL
+);
+
+-- Create Booking table
+CREATE TABLE IF NOT EXISTS Bookings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    package TEXT NOT NULL,
+    name TEXT NOT NULL,
+    date TEXT NOT NULL,
+    time TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone TEXT NOT NULL
+);
+
+COMMIT;
